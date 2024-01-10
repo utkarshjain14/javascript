aeadfea9507a43a99bbe0fccc4c609cef50637e2
utkarshjain14 <109435070+utkarshjain14@users.noreply.github.com>
2024-01-10 18:08:25 +0000

    data

---
+++ 01basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(3+5)
+console.log("utkarsh")
+
+let name = "utkarsh"
+let age = 20
+let isLoggedIn = false 
+let state = null
+// primitive data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => stand alone value 
+// undefined => 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
+
+
