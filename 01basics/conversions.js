bebb3b0f7eee2d6883afb7a70c2a535157c26a94
utkarshjain14 <109435070+utkarshjain14@users.noreply.github.com>
2024-01-10 18:48:05 +0000

    conversions and operations

--- 01basics/conversions.js
+++ 01basics/conversions.js
@@ -17,7 +17,7 @@ let isLoggedIn = "utkarsh"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,10 +26,47 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
 // *********************** Operations ***********************
 
+
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "utkarsh"
+let str2 = " hello"
+ let str3 = str1 + str2
+ console.log(str3);
+
+
+//  console.log("1" + 2);
+// console.log(1 + "2"); 
+// console.log("1" + "2"); 
+// console.log("1" + 2 + 2); 
+// console.log(1 + 2 + "2"); 
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
