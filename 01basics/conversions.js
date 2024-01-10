b1a4651e0736de36d978587f357e4105e3563cfc
utkarshjain14 <109435070+utkarshjain14@users.noreply.github.com>
2024-01-10 18:29:48 +0000

    conversions of datatypes

---
+++ 01basics/conversions.js
@@ -0,0 +1,35 @@
+let score = true
+//console.log(typeof score) // number 
+
+// let age = "33"
+// console.log(typeof age) // string
+
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+// console.log(score)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "utkarsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// *********************** Operations ***********************
+
