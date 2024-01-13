3b31bcd5ba1f3b40e9cb4cc619f0d48cebd0c3be
utkarshjain14 <109435070+utkarshjain14@users.noreply.github.com>
2024-01-13 12:25:29 +0000

    functions

---
+++ 03basics/functions.js
@@ -0,0 +1,27 @@
+function sayMyName(){
+    console.log("U");
+    console.log("T");
+    console.log("K");
+    console.log("A");
+    console.log("R");
+    console.log("S");
+    console.log("H");
+}
+
+sayMyName();
+
+// function addTwoNumbers(){
+
+//     console.log(Number1 + Number2);
+    
+// }
+
+function addTwoNumbers(){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+console.log("Result: ", result);
